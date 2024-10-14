78a029be63298904077845476208a4fd8b385729
Quix2872 <raymirza9@gmail.com>
2024-10-13 21:14:47 -0500

    Add files via upload

---
+++ cybernetic-organs-system-assignment-6/src/main/java/com/cybernetic/Patient.java
@@ -0,0 +1,20 @@
+package com.cybernetic;
+
+public class Patient {
+    private String id;
+    private String bloodType;
+    private int weight;
+    private String hlaType;
+
+    public Patient(String id, String bloodType, int weight, String hlaType) {
+        this.id = id;
+        this.bloodType = bloodType;
+        this.weight = weight;
+        this.hlaType = hlaType;
+    }
+
+    public String getId() { return id; }
+    public String getBloodType() { return bloodType; }
+    public int getWeight() { return weight; }
+    public String getHlaType() { return hlaType; }
+}
